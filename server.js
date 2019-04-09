45f1ddefdc70dbbde9fe61a4113189765aeb23e0
Arsen4ik <46644855+Arsen4ik@users.noreply.github.com>
2019-04-09 19:00:27 +0300

    added signin

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const bodyParser =require('body-parser')
+
+
+const app = express();
+app.use(bodyParser.json())
+
+const database = {
+    users: [
+        {
+            id: '123',
+            name: 'John',
+            email: 'john@gmail.com',
+            password: 'cookies',
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id: '124',
+            name: 'Sally',
+            email: 'Sally@gmail.com',
+            password: 'bananas',
+            entries: 0,
+            joined: new Date()
+        }
+    ]
+}
+
+app.get('/', (req, res) => {
+    
+    res.send('working');
+})
+
+app.post('/signin', (req, res) => {
+    if(req.body.email === database.users[0].email && 
+        req.body.password === database.users[0].password) {
+           res.json('success'); 
+        } else {
+            res.status(400).json('error loading')
+        }
+})
+
+app.listen(3000, ()=> {
+    console.log('runnin on 3000');
+})

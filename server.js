645c2c6e1caabd0ba183e7a70ad825b28c050f5b
Arsen4ik <46644855+Arsen4ik@users.noreply.github.com>
2019-04-12 08:56:42 +0300

    profile finished

--- server.js
+++ server.js
@@ -85,16 +85,19 @@ app.post('/register', (req, res) => {
 //profile
 app.get('/profile/:id', (req, res) => {
     const { id } = req.params;
-    let found = false;
-    database.users.forEach(user => {
-        if (user.id === id) {
-            found = true;
-          return res.json(user) 
-        }
-    })
-    if (!found) {
-        res.status(400).json('not found')
-    }
+    
+    db.select('*').from('users')
+        .where({
+            id: id
+        })
+        .then(user => {
+            if(user.length){
+            res.json(user[0])
+            } else {
+                res.status(400).json('not found') 
+            }
+        })
+        .catch(err => res.status(400).json('error getting user'))
 })
 
 //image entries count
